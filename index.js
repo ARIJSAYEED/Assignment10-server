252b735d6576a1e0d11d2c0225b442db68c0a732
ARIJSAYEED <arijsayeed@gmail.com>
2025-11-29 13:50:53 +0600

    stopped the duplication

--- index.js
+++ index.js
@@ -84,8 +84,10 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/myimports',async(req,res)=>{
-            const cursor = importedProducts.find();
+        app.get('/myimports', async (req, res) => {
+            const email = req.query.email;
+            const query = { buyer_email: email }
+            const cursor = importedProducts.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
